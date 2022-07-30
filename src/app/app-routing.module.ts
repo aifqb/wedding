095a5baf3860ac94507d8169955b1e59da620cc6
aifqb <alexquisbert@gmail.com>
2022-07-29 20:36:32 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'birthday',
+    path: 'miguel',
     loadChildren: () => import('./mapas/mapas.module').then( m =>m.MapasModule)
   },
   {
     path:'**',
-    redirectTo: 'birthday'
+    redirectTo: 'miguel'
   }
 ];
 
